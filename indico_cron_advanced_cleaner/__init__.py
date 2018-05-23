657a1644e0ae9058c91beb8deeeecba7c15cf655
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2018-05-23 14:42:19 +0200

    Ensure list_plugins works

--- indico_cron_advanced_cleaner/__init__.py
+++ indico_cron_advanced_cleaner/__init__.py
@@ -1,14 +1,13 @@
-# This file is part of the CERN Indico plugins.
-# Copyright (C) 2014 - 2018 CERN
+# This file is part of the MLZ Indico plugins.
+# Copyright (C) 2018 MLZ
 #
-# The CERN Indico plugins are free software; you can redistribute
+# The MLZ Indico plugins are free software; you can redistribute
 # them and/or modify them under the terms of the MIT License; see
 # the LICENSE file for more details.
 
 from __future__ import unicode_literals
 
 from indico.core import signals
-from indico.modules.events.features.base import EventFeature
 
 
 @signals.import_tasks.connect
@@ -17,15 +16,6 @@ def _import_tasks(sender, **kwargs):
 
 @signals.event.get_feature_definitions.connect
 def _get_feature_definitions(sender, **kwargs):
+    from .features import CleanLogFeature, AnonymizeFeature
     yield CleanLogFeature
     yield AnonymizeFeature
-
-class CleanLogFeature(EventFeature):
-    name = 'cleanup_log'
-    friendly_name = _('Cleanup Logs')
-    description = _('Let a cron task cleanup the log after a certain period.')
-
-class AnonymizeFeature(EventFeature):
-    name = 'anonymize_registrations'
-    friendly_name = _('Anonymize Registrations')
-    description = _('Let a cron task anonymize registrations after a certain period.')
---
+++ indico_cron_advanced_cleaner/features.py
@@ -0,0 +1,19 @@
+# This file is part of the MLZ Indico plugins.
+# Copyright (C) 2018 MLZ
+#
+# The MLZ Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
+from indico.modules.events.features.base import EventFeature
+
+
+class CleanLogFeature(EventFeature):
+    name = 'cleanup_log'
+    friendly_name = _('Cleanup Logs')
+    description = _('Let a cron task cleanup the log after a certain period.')
+
+class AnonymizeFeature(EventFeature):
+    name = 'anonymize_registrations'
+    friendly_name = _('Anonymize Registrations')
+    description = _('Let a cron task anonymize registrations after a certain period.')
